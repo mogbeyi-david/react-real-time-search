53581c842485d01ed051434ca9cad018be9a75fc
DavidMogbeyi <davidm@l5lab.com>
2018-10-30 10:35:08 +0100

    Added content to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-
+  render(){
+    return (
+      <div className="header">
+        <h1>React Search</h1>
+        <p>Here is a list of Reggae artists rendered from a JSON object</p>
+      </div>
+    )
+  }
 }
 
 export default Header;
